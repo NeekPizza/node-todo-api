0a1ff0abf7ead704c629bb5e93a1fd0c0cc2437f
ReadyDoMe <unitecomrades777@gmail.com>
2019-02-20 23:13:32 -0600

    init - created connection to mongodb, created collections, inserted data

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,32 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Error connecting to the MongoDB server');
+  }
+  console.log('Connected to MongoDB')
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false,
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert Todo', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  db.collection('Users').insertOne({
+    name: 'Nick',
+    age: 28,
+    location: 'Austin, Tx',
+  }, (err, result) => {
+    if (err) {
+      return console.log('Error saving user', err);
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  })
+
+  client.close();
+});
